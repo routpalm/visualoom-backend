7b52d17f788442d95e5e9d6e66cd9f4efd682f9d
Brett DeWitt <bdewitt1984@gmail.com>
2024-11-02 20:46:20 -0700

    Fix: case sensitivity error importing authController in authRoute.js, was causing deployment failure

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/AuthController');
+const authController = require('../controllers/authController');
 
 
 // Redirect user to google login
