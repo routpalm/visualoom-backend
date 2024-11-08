a9d696040c0d8e2deb5e3d6c2e8f61a114153fdb
Brett DeWitt <bdewitt1984@gmail.com>
2024-11-08 14:17:08 -0800

    Added: initialization for new model definitions

--- server/models/index.js
+++ server/models/index.js
@@ -6,17 +6,20 @@ const sequelize = require('../config/setupSequelize');
 
 // Import model definitions and initialize
 const models = {
+  Artwork: Artwork = require('./Artwork')(sequelize),
   HelloWorld: HelloWorld = require('./HelloWorld')(sequelize),
-  User: User = require('./UserModel')(sequelize),
-  Like: Like = require('./LikeModel')(sequelize),
+  Like: Like = require('./Like')(sequelize),
+  Params: Params = require('./Params')(sequelize),
+  User: User = require('./User')(sequelize)
 }
 
 // Define relationships
-Object.values(models).forEach((model) => {
-  if (model.associate) {
-    model.associate(models);
-  }
-});
+// Object.values(models).forEach((model) => {
+//   if (model.associate) {
+//     model.associate(models);
+//   }
+// });
+
 
 // Export the Sequelize and model instances
 module.exports = {
